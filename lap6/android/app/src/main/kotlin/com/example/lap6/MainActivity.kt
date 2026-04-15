c35bf001ce772c6dfa27d989e3239404606c906a
chau2512 <chau2512@users.noreply.github.com>
2026-04-16 00:45:39 +0700

    Add Lab 6: Quizzler App

---
+++ lap6/android/app/src/main/kotlin/com/example/lap6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lap6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lap6/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'Flutter là một framework của Google',
+    'RAM là bộ nhớ lưu trữ dữ liệu vĩnh viễn',
+    'Dart là ngôn ngữ lập trình chính được sử dụng trong Flutter',
+    'Trái đất quay quanh mặt trời',
+    '1 + 1 = 3',
+  ];
+
+  List<bool> answers = [
+    true,
+    false,
+    true,
+    true,
+    false,
+  ];
+
+  int questionNumber = 0;
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = answers[questionNumber];
+
+    setState(() {
+      if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(const Icon(Icons.close, color: Colors.red));
+      }
+
+      if (questionNumber < questions.length - 1) {
+        questionNumber++;
+      } else {
+        // Reset quiz when finished
+        questionNumber = 0;
+        scoreKeeper.clear();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        // Score Keeper
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
